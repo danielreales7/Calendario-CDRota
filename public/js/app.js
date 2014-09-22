433174e4e41b49dfcbdb15bbb6d09dc429b7bbf8
Daniel <danielreales7@gmail.com>
2014-09-22 18:47:46 +0200

    Funcionalidad añadida a la directiva de comentarios

--- public/js/app.js
+++ public/js/app.js
@@ -20,28 +20,6 @@
 		};
 	});
 
-	app.controller('CommentsController', function(){
-		this.comments = []; //Vamos a inicializar un arreglo donde vamos a ir guardando los comentarios
-		this.comment = {}; //Aquí vamos almacenar los elementos del formulario
-		this.show = false; //Con esto vamos a saber si nuestro panel se está mostrando o está oculto
-		// Con esta función estamos cambiando de estado un valor Booleano
-		this.toggle = function(){
-			this.show = !this.show;
-		};
-
-		this.anonymousChanged = function(){
-			if(this.comment.anonymous){
-				this.comment.email = "";
-			}
-		};
-
-		this.addComment = function(){
-			this.comment.date = Date.now();
-			this.comments.push(this.comment);
-			this.comment = {};
-		};
-	});
-
 	app.directive('partidoEquipos', function(){
 		return {
 			restrict: 'E', //Esta directiva quiere decir que estamos creando un elemento HTML
@@ -75,6 +53,35 @@
 		return {
 			restrict: 'E', //Esta directiva quiere decir que estamos creando un elemento HTML
 			templateUrl: 'partials/partido-ejemplo.html'
+		};
+	});
+
+	app.directive('partidoComments', function(){
+		return {
+			restrict: 'E', //Esta directiva quiere decir que estamos creando un elemento HTML
+			templateUrl: 'partials/partido-comments.html',
+			controller: function(){
+				this.comments = []; //Vamos a inicializar un arreglo donde vamos a ir guardando los comentarios
+				this.comment = {}; //Aquí vamos almacenar los elementos del formulario
+				this.show = false; //Con esto vamos a saber si nuestro panel se está mostrando o está oculto
+				// Con esta función estamos cambiando de estado un valor Booleano
+				this.toggle = function(){
+					this.show = !this.show;
+				};
+
+				this.anonymousChanged = function(){
+					if(this.comment.anonymous){
+						this.comment.email = "";
+					}
+				};
+
+				this.addComment = function(){
+					this.comment.date = Date.now();
+					this.comments.push(this.comment);
+					this.comment = {};
+				};
+			},
+			controllerAs: 'cmtsCtrl'
 		};
 	});
 
