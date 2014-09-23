63d2ea1be778170d42228d4407d2e889a6426e5e
Daniel <danielreales7@gmail.com>
2014-09-23 17:32:38 +0200

    Añadimos ngRoute y rutas básicas para cargar los partidos

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,17 @@
 (function (){
 	var app = angular.module('calendario', [
+		'ngRoute',
 		'calendario.controllers',
 		'calendario.directives',
 		'calendario.filters'
 	]); //Nuestro módulo ahora mismo no tiene dependencias por eso ponemos []
+	//Creamos nuestra inyección de dependencias $routeProvider
+	app.config(['$routeProvider', function($routeProvider){
+		$routeProvider
+			.when('/', {
+				templateUrl: 'views/partidos.html',
+				controller: 'EquiposController',
+				controllerAs: 'equiposCtrl'
+			})
+	}]);
 })();
